97026eeaf75e6a90e93404501c3f92e16b629795
YeldarOralov <YeldarOralov@users.noreply.github.com>
2019-03-06 11:57:58 +0600

    First commit

--- SerializationApp/Program.cs
+++ SerializationApp/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
 
 namespace SerializationApp
 {
@@ -10,6 +13,33 @@ namespace SerializationApp
     {
         static void Main(string[] args)
         {
+            Person person = new Person
+            {
+
+            };
+          
+            string json = JsonConvert.SerializeObject(person);
+            Person result = JsonConvert.DeserializeObject<Person>(json);
+
+
+
+
+            WebClient client = new WebClient();
+            var jsonChar = client.DownloadString("http://hp-api.herokuapp.com/api/characters");
+            var rez = JsonConvert.DeserializeObject<RootObject[]>(jsonChar);
+
+            
+            jsonChar = client.DownloadString("http://hp-api.herokuapp.com/api/characters/students");
+            rez = JsonConvert.DeserializeObject<RootObject[]>(jsonChar);
+
+            jsonChar = client.DownloadString("http://hp-api.herokuapp.com/api/characters/staff");
+            rez = JsonConvert.DeserializeObject<RootObject[]>(jsonChar);
+
+            jsonChar = client.DownloadString("http://hp-api.herokuapp.com/api/characters/house/gryffindor");
+            rez = JsonConvert.DeserializeObject<RootObject[]>(jsonChar);
+
+            Console.WriteLine("Файл загружен");
+            Console.Read();
         }
     }
 }
